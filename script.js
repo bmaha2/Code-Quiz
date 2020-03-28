8280ebd254d1b458c9fa1a4b3fcab0f20ff796f0
bmaha2 <maharjanbipin@gmail.com>
2020-03-27 20:13:30 -0700

    got questions and answers displaying

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ timer
 current question no
 score correct/incorrect
 
-
 when game starts 
 curr ques = 0
 timer 60;
@@ -38,55 +37,51 @@ if (true) {
     handle end of game.
 }
 
-
 end of  game;
 display scores
 ask if they want to play again
-
-
-
  
  */
 
 var questions = [
     {
-        text: "what is a full-stack?",
+        text: "What is a full-stack?",
         answers: [
-            { text: "HTML", correct: false },
-            { text: "Back-end Front-end", correct: true },
-            { text: "CSS", correct: false },
-            { text: "JavaScipt", correct: false },
+            { text: "1.HTML", correct: false },
+            { text: "2.Back-end Front-end", correct: true },
+            { text: "3.CSS", correct: false },
+            { text: "4.JavaScipt", correct: false },
 
         ]
 
     },
     {
-        text: "example question2",
+        text: "What is CSS ?",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: false },
-            { text: "ans 3", correct: true },
-            { text: "ans 4", correct: false },
+            { text: "1.Component Style Sheet", correct: false },
+            { text: "2.Current Style Sheet", correct: false },
+            { text: "3.Cascading Style Sheet", correct: true },
+            { text: "4.Charactes Style Sheet", correct: false },
 
         ]
     },
     {
-        text: "example question3",
+        text: "What does HTML stands for?",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: true },
-            { text: "ans 3", correct: false },
-            { text: "ans 4", correct: false },
+            { text: "1.", correct: false },
+            { text: "2.", correct: true },
+            { text: "3.", correct: false },
+            { text: "4.", correct: false },
 
         ]
     },
     {
         text: "example question4",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: true },
-            { text: "ans 3", correct: false },
-            { text: "ans 4", correct: false },
+            { text: "1.", correct: false },
+            { text: "2.", correct: true },
+            { text: "3.", correct: false },
+            { text: "4.", correct: false },
 
         ]
 
@@ -94,10 +89,10 @@ var questions = [
     {
         text: "example question5",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: true },
-            { text: "ans 3", correct: false },
-            { text: "ans 4", correct: false },
+            { text: "1.", correct: false },
+            { text: "2.", correct: true },
+            { text: "3.", correct: false },
+            { text: "4.", correct: false },
 
         ]
 
@@ -105,40 +100,40 @@ var questions = [
     {
         text: "example question6",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: true },
-            { text: "ans 3", correct: false },
-            { text: "ans 4", correct: false },
+            { text: "1.", correct: false },
+            { text: "2.", correct: true },
+            { text: "3.", correct: false },
+            { text: "4.", correct: false },
 
         ]
     },
     {
         text: "example question7",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: true },
-            { text: "ans 3", correct: false },
-            { text: "ans 4", correct: false },
+            { text: "1.", correct: false },
+            { text: "2.", correct: true },
+            { text: "3.", correct: false },
+            { text: "4.", correct: false },
 
         ]
     },
     {
         text: "example question8",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: true },
-            { text: "ans 3", correct: false },
-            { text: "ans 4", correct: false },
+            { text: "1.", correct: false },
+            { text: "2.", correct: true },
+            { text: "3.", correct: false },
+            { text: "4.", correct: false },
 
         ]
     },
     {
         text: "example question9",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: true },
-            { text: "ans 3", correct: false },
-            { text: "ans 4", correct: false },
+            { text: "1.", correct: false },
+            { text: "2.", correct: true },
+            { text: "3.", correct: false },
+            { text: "4.", correct: false },
 
         ]
     }
@@ -146,10 +141,10 @@ var questions = [
     {
         text: "example question10",
         answers: [
-            { text: "ans 1", correct: false },
-            { text: "ans 2", correct: true },
-            { text: "ans 3", correct: false },
-            { text: "ans 4", correct: false },
+            { text: "1.", correct: false },
+            { text: "2.", correct: true },
+            { text: "3.", correct: false },
+            { text: "4.", correct: false },
 
         ]
     }
@@ -158,146 +153,159 @@ var questions = [
 //varibles to get hold of the nodes
 var scoreEl = document.getElementById("high-score"); //for score
 var timerEl = document.getElementById("timer")       //for timer
-var startEl = document.getElementById("start");      // div for main display excluding timer and score
 var headerEl = document.querySelector(".header");    //div for score and timer
 var wrapperEl = document.querySelector(".wrapper");  //for styling 
 var boxEl = document.querySelector(".inside-box");   //for styling
 var activeEl = document.querySelector(".active");
+var displayEl = document.getElementById("display");      // div for main display excluding timer and score
+var questionHeadEl = document.getElementById("question-head");
+var answerListEl = document.getElementById("l.ist");
+var messageEl = document.getElementById("message-container");
 
 //initial setup
-var gameTimer = 60;
+var time = 60;
+var gameTimer;
 var currentQuestionIndex = 0;
-
 var score = 0;
 
-//wrapper to display the DOM
-function wrapper() {
-    //body(wrapper)
-    //wrapper(header, inside-box)
-    //header(score, timer)
-    boxEl.appendChild(startEl);
-    wrapperEl.appendChild(headerEl);
-    headerEl.appendChild(scoreEl);
-    headerEl.appendChild(timerEl);
-    wrapperEl.appendChild(boxEl);
-    document.body.appendChild(wrapperEl);
-}
+
 
 //Display info initial page
 function displayInfo() {
-
     //varibles to create tags to display when the app starts    
     var h2El = document.createElement("h2");       //h2 for to display play
     var pEl = document.createElement("p");         //info of game
     var btnEl = document.createElement("button")   //to start the game
-
+    btnEl.setAttribute("id", "start");
     //text contents of the corresponding tags
     h2El.textContent = "Play the code quiz game";
     pEl.textContent = "The game has 10 questions and you have 60 seconds to complete the game.If wrong answer is selected you will lose time by 5 seconds. Have fun!";
     btnEl.textContent = "START";
-
-
     //inside-box (h2, p and start button)    
-    startEl.appendChild(h2El);
-    startEl.appendChild(pEl);
-    startEl.appendChild(btnEl);
-    wrapper();
+    displayEl.appendChild(h2El);
+    displayEl.appendChild(pEl);
+    displayEl.appendChild(btnEl);
+    boxEl.appendChild(displayEl);
+    btnEl.addEventListener("click", listQuestion);
+    btnEl.addEventListener("click", Timer);
+
 }
-//calling displayInfo() for displaying the fisrt page
-displayInfo();
-
-//event handler set to start button to start the game
-startEl.addEventListener("click", function (e) {
-    //game timer    
-    setInterval(() => {
-        timerEl.textContent = "Time Left: " + gameTimer;
-        gameTimer--;
+function Timer() {
+    gameTimer = setInterval(function () {
+        timerEl.textContent = "Time Left: " + time;
+        time--;
+        if (time < 1) {
+            endGame();
+            // return;
+        }
     }, 1000);
-    
-        listQuestion(currentQuestionIndex);
-    
+
+}
+
+//calling displayInfo() for displaying the first page
+displayInfo();
+function listQuestion() {
 
 
-});
-function listQuestion(currentQuestionIndex) {
+    var currentQuestion = questions[currentQuestionIndex];
+    console.log(currentQuestion);
+    if (!currentQuestion) {
+        return endGame();
+    }
+    displayEl.style.display = "none";
+    var questionDiv = createQuestion(currentQuestion);
 
-    startEl.style.display = "none";
-    var obj = questions[currentQuestionIndex];
+
+    boxEl.prepend(questionDiv);
+    //to display whether the answer is correct or incorrect in a label
+    //var answerListEl = document.getElementById("l.ist");
+
+}
+function createQuestion(currentQuestion) {
     var divQuestion = document.createElement("div");
     divQuestion.setAttribute("class", "question");
     var questionHead = document.createElement("h2");
     questionHead.setAttribute("id", "question-head");
-    questionHead.textContent = obj.text;
+    questionHead.textContent = currentQuestion.text;
     divQuestion.appendChild(questionHead);
     var answerList = document.createElement("div");
-    answerList.setAttribute("id", "ans-list");
+    answerList.setAttribute("id", "l.ist");
 
-
-    for (var i = 0; i < obj.answers.length; i++) {
+    for (var i = 0; i < currentQuestion.answers.length; i++) {
         var btn = document.createElement("button");
+
         btn.setAttribute("id", "btn" + [i]);
-        btn.textContent = btn.textContent + obj.answers[i].text;
-        if (obj.answers[i].correct) {
+        btn.textContent = btn.textContent + currentQuestion.answers[i].text;
+        if (currentQuestion.answers[i].correct) {
             btn.setAttribute("class", "true");
         }
         answerList.appendChild(btn);
     }
-
-
     divQuestion.appendChild(answerList);
-    boxEl.appendChild(divQuestion);
-    var answerListEl = document.getElementById("ans-list")
-
-
-
-    answerListEl.addEventListener("click", function (e) {
-        e.preventDefault();
-        if (e.target.nodeName == "BUTTON") {
-            displayRightWrong();
-            var rightWrongEl = document.querySelector(".rightWrong");
-            // alert(e.target.textContent);
-            if (e.target.hasAttribute("class", "true")) {
-                // alert("CORRECT!");
-                rightWrongEl.textContent = "Correct!"
-                score++;
-                scoreEl.textContent = "Score: " + score;
-            }
-            else {
-                //alert("WRONG!")
-                rightWrongEl.textContent = "Wrong!";
-            }
+   // var divQuestionEl = document.querySelector(".question");
+    divQuestion.addEventListener("click", function (event) {
+        event.preventDefault();
+        checkAnswer(event.target);
+        divQuestion.parentNode.removeChild(divQuestion);
+        if (currentQuestionIndex < questions.length) {
+            currentQuestionIndex++;
+            listQuestion();
         }
-        
-        clearCurrentQuestion();
-        currentQuestionIndex++;
-        nextQuestion(currentQuestionIndex);
     });
+    return divQuestion;
 
 }
-//to display whether the answer is correct or incorrect in a label
-function displayRightWrong() {    
-    var rightWrong = document.createElement("label");
-    rightWrong.setAttribute("class", "rightWrong");    
-    boxEl.appendChild(rightWrong);
+
+function checkAnswer(target) {
+    clearMessage();
+    var selectedAnswer = document.createElement("label");
+    selectedAnswer.setAttribute("class", "selected");
+
+    if (target.matches("BUTTON")) {
+
+
+        //alert(e.target.textContent);
+
+        if (target.hasAttribute("class", "true")) {
+            // alert("CORRECT!");
+            selectedAnswer.textContent = "Correct!"
+            score++;
+            scoreEl.textContent = "Score: " + score;
+        }
+        else {
+            //alert("WRONG!")
+            selectedAnswer.textContent = "Wrong!";
+        }
+
+    }
+    messageEl.appendChild(selectedAnswer);
+
+
 }
 
-//to clear current question
-function clearCurrentQuestion() {
-    var divQuestionEl = document.querySelector(".question");
-    divQuestionEl.parentNode.removeChild(divQuestionEl);
-    
+function clearMessage() {
+    messageEl.innerHTML = "";
+}
+function endGame() {
+    clearMessage();
+    //stop the timer
+    clearInterval(gameTimer);
+    finalScore();
 
 }
 //to display nextQuestion
-function nextQuestion(currentQuestionIndex) {
-    if (currentQuestionIndex <= questions.length){
-        listQuestion(currentQuestionIndex);
-    }
-    
-}
+// function nextQuestion() {
+//     questionHead.parentNode.removeChild(questionHead);
+//     answerList.parentNode.removeChild(answerList);
 
+//     if (currentQuestionIndex < questions.length) {
+//         listQuestion();
+//     }
+//     if (currentQuestionIndex >= questions.length) {
+//         finalScore();
+//     }
+// }
 //to display final score
 function finalScore() {
-    alert("Your Score");
-
+    alert(scoreEl.textContent);
 }
