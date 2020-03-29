0c71081212f11abbfa455b2bb3d62d3da5030d57
bmaha2 <maharjanbipin@gmail.com>
2020-03-28 22:50:08 -0700

    final

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //hold questions and answers
+//questions are from tutorialspoint.com
 var questions = [
     {
         text: "What is a full-stack?",
         answers: [
-            { text: "1.HTML", correct: false },
-            { text: "2.Back-end Front-end", correct: true },
-            { text: "3.CSS", correct: false },
-            { text: "4.JavaScipt", correct: false },
+            { text: "1. HTML", correct: false },
+            { text: "2 .Back-end and Front-end", correct: true },
+            { text: "3. CSS", correct: false },
+            { text: "4. JavaScipt", correct: false },
 
         ]
 
@@ -14,93 +15,93 @@ var questions = [
     {
         text: "What is CSS ?",
         answers: [
-            { text: "1.Component Style Sheet", correct: false },
-            { text: "2.Current Style Sheet", correct: false },
-            { text: "3.Cascading Style Sheet", correct: true },
-            { text: "4.Charactes Style Sheet", correct: false },
+            { text: "1. Component Style Sheet", correct: false },
+            { text: "2. Current Style Sheet", correct: false },
+            { text: "3. Cascading Style Sheet", correct: true },
+            { text: "4. Charactes Style Sheet", correct: false },
 
         ]
     },
     {
         text: "What does HTML stands for?",
         answers: [
-            { text: "1.", correct: false },
-            { text: "2.", correct: true },
-            { text: "3.", correct: false },
-            { text: "4.", correct: false },
+            { text: "1. Hyper Text Markup Language", correct: true },
+            { text: "2. Home Text Markup Language", correct: false },
+            { text: "3. Hyperlinks Text Markup Language", correct: false },
+            { text: "4. Hyper Tool Markup Language", correct: false },
 
         ]
     },
     {
-        text: "example question4",
+        text: "What is the coorect HTML tag for largest heading?",
         answers: [
-            { text: "1.", correct: false },
-            { text: "2.", correct: true },
-            { text: "3.", correct: false },
-            { text: "4.", correct: false },
-
-        ]
-
-    },
-    {
-        text: "example question5",
-        answers: [
-            { text: "1.", correct: false },
-            { text: "2.", correct: true },
-            { text: "3.", correct: false },
-            { text: "4.", correct: false },
+            { text: "1. H6", correct: false },
+            { text: "2. H1", correct: true },
+            { text: "3. Heading", correct: false },
+            { text: "4. Head", correct: false },
 
         ]
 
     },
     {
-        text: "example question6",
+        text: "How to get the type of arguments passed to a fucntion?",
         answers: [
-            { text: "1.", correct: false },
-            { text: "2.", correct: true },
-            { text: "3.", correct: false },
-            { text: "4.", correct: false },
+            { text: "1. using typeof operator", correct: true },
+            { text: "2. using getType function", correct: false },
+            { text: "3. Both of the above", correct: false },
+            { text: "4. none of the above", correct: false },
+
+        ]
+
+    },
+    {
+        text: "Which of the following function of String object returns the character at the specified index",
+        answers: [
+            { text: "1. charIndexAt", correct: false },
+            { text: "2. charAt", correct: true },
+            { text: "3. indexof", correct: false },
+            { text: "4. none of the above", correct: false },
 
         ]
     },
     {
-        text: "example question7",
+        text: "Which of the following type of variable is visible only within a function where it is defined?",
         answers: [
-            { text: "1.", correct: false },
-            { text: "2.", correct: true },
-            { text: "3.", correct: false },
-            { text: "4.", correct: false },
+            { text: "1. local variable", correct: true },
+            { text: "2. global variable", correct: false },
+            { text: "3. both of above", correct: false },
+            { text: "4. None of above", correct: false },
 
         ]
     },
     {
-        text: "example question8",
+        text: "Which built-in method sorts the elements of an array?",
         answers: [
-            { text: "1.", correct: false },
-            { text: "2.", correct: true },
-            { text: "3.", correct: false },
-            { text: "4.", correct: false },
+            { text: "1. changeOrder(order)", correct: false },
+            { text: "2. sort()", correct: true },
+            { text: "3. order()", correct: false },
+            { text: "4. None of the above", correct: false },
 
         ]
     },
     {
-        text: "example question9",
+        text: "Which of the following function of String object returns a string representing the specified object?",
         answers: [
-            { text: "1.", correct: false },
-            { text: "2.", correct: true },
-            { text: "3.", correct: false },
-            { text: "4.", correct: false },
+            { text: "1. toLocaleUpperCase()", correct: false },
+            { text: "2. substring()", correct: false },
+            { text: "3. toUpperCase()", correct: false },
+            { text: "4. toString()", correct: true },
 
         ]
     }
     ,
     {
-        text: "example question10",
+        text: "Which of the following property is used to add or subtract space between the letters that make up a word?",
         answers: [
-            { text: "1.", correct: false },
-            { text: "2.", correct: true },
-            { text: "3.", correct: false },
-            { text: "4.", correct: false },
+            { text: "1. word-spacing", correct: false },
+            { text: "2. direction", correct: false },
+            { text: "3. letter-spacing", correct: true },
+            { text: "4. color", correct: false },
 
         ]
     }
@@ -109,13 +110,9 @@ var questions = [
 //varibles to get hold of the nodes
 var scoreEl = document.getElementById("high-score"); //for score
 var timerEl = document.getElementById("timer")       //for timer
-var headerEl = document.querySelector(".header");    //div for score and timer
 var wrapperEl = document.querySelector(".wrapper");  //for styling 
 var boxEl = document.querySelector(".inside-box");   //for styling
-var activeEl = document.querySelector(".active");
-var displayEl = document.getElementById("display");      // div for main display excluding timer and score
-var questionHeadEl = document.getElementById("question-head");
-var answerListEl = document.getElementById("ans-list");
+var displayEl = document.getElementById("display");   // div for main display excluding timer and score
 var messageEl = document.getElementById("message-container");
 
 //initial setup
@@ -154,9 +151,10 @@ function Timer() {
         }
     }, 1000);
 }
-
 //calling startGame
 startGame();
+
+//to list the questions
 function listQuestion() {
     var currentQuestion = questions[currentQuestionIndex];
     if (!currentQuestion) {
@@ -166,6 +164,7 @@ function listQuestion() {
     var questionDiv = createQuestion(currentQuestion);
     boxEl.prepend(questionDiv);
 }
+//function to create question with possible answers
 function createQuestion(currentQuestion) {
     var divQuestion = document.createElement("div");
     divQuestion.setAttribute("class", "question");
@@ -175,10 +174,8 @@ function createQuestion(currentQuestion) {
     divQuestion.appendChild(questionHead);
     var answerList = document.createElement("div");
     answerList.setAttribute("id", "ans-list");
-
     for (var i = 0; i < currentQuestion.answers.length; i++) {
         var btn = document.createElement("button");
-
         btn.setAttribute("id", "btn" + [i]);
         btn.textContent = btn.textContent + currentQuestion.answers[i].text;
         if (currentQuestion.answers[i].correct) {
@@ -198,7 +195,7 @@ function createQuestion(currentQuestion) {
     });
     return divQuestion;
 }
-
+//function to check answer 
 function checkAnswer(target) {
     clearMessage();
     var selectedAnswer = document.createElement("label");
@@ -233,7 +230,7 @@ function endGame() {
 //to display final score
 function finalScore() {
     //alert(scoreEl.textContent);
-    var doneDiv = document.createElement("div");    
+    var doneDiv = document.createElement("div");
     var allDone = document.createElement("h2");
     allDone.textContent = "ALL DONE!"
     var finalScoreDisp = document.createElement("p");
@@ -246,7 +243,7 @@ function finalScore() {
     inputName.setAttribute("type", "text");
     var submitBtn = document.createElement("button");
     submitBtn.textContent = "Submit";
-   doneDiv.appendChild(allDone);
+    doneDiv.appendChild(allDone);
     doneDiv.appendChild(finalScoreDisp);
     formSubmit.appendChild(inputNameField);
     formSubmit.appendChild(inputName);
@@ -256,25 +253,18 @@ function finalScore() {
     submitBtn.addEventListener("click", function (e) {
         e.preventDefault();
         doneDiv.parentNode.removeChild(doneDiv);
-        
-        
         var highScoreDiv = document.createElement("div");
         highScoreDiv.setAttribute("id", "high-score");
         highScore = document.createElement("h2");
         highScore.textContent = "High Scores";
         highScoreDiv.appendChild(highScore);
         var scoreList = document.createElement("label");
-        
-        
-        
         inputName.value = inputName.value + " - " + score;
         //set new submission
         localStorage.setItem("user", JSON.stringify(inputName.value));
         //get recent submission
-        
         scoreList.textContent = JSON.parse(localStorage.getItem("user"));
         highScoreDiv.appendChild(scoreList);
-        
         var goBackBtn = document.createElement("button");
         goBackBtn.textContent = "Go Back";
         var clearScore = document.createElement("button");
@@ -283,22 +273,13 @@ function finalScore() {
         highScoreDiv.appendChild(clearScore);
         boxEl.appendChild(highScoreDiv);
 
-        clearScore.addEventListener("click", function(e){
+        clearScore.addEventListener("click", function (e) {
             e.preventDefault();
             scoreList.textContent = "";
-
         })
         goBackBtn.addEventListener("click", function (e) {
             e.preventDefault();
-            restartGame();
-            
-        })
-
-
-    })
-  
-}function restartGame() {
-    startGame();
+            startGame();
+        });
+    });
 }
-
-  
